b710cdfdca4116a49411218135f09ee5b7bac156
Dubemxolisi <dubemxolisi04@gmail.com>
2023-02-20 01:04:37 -0800

    100 intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  * main - prgram that prints a size of various types of computer
  * Return 0 (success)
  */
+
 int main(void)
 {
 char a;
@@ -17,5 +18,5 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-return ('0');
+return (0);
 }
