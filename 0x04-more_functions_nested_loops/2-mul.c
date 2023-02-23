7d9b33ba829c06faaa11af43864afa17d9b87391
Dubemxolisi <dubemxolisi04@gmail.com>
2023-02-23 10:12:03 -0800

    multiplie

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplie to intergers
+ * @x: the first number to multiplie
+ * @y: the second number to multiplie with the first number
+ * Retran: the result of multiplication between x param and y param
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
