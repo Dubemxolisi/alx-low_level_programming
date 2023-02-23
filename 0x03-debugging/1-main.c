c9814702cbcfa1000f3613a03fa0d4154c72de95
Dubemxolisi <dubemxolisi04@gmail.com>
2023-02-23 00:01:05 -0800

    infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinit loop
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i:
+
+	printf("infinite loop incoming :(\n");
+
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+
+}
