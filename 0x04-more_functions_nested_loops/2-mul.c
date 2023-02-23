c2e0e54bb28be71fa5a6430299435184a284b8cd
Dubemxolisi <dubemxolisi04@gmail.com>
2023-02-23 10:38:30 -0800

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * mul - multiplie to intergers
- * @x: the first number to multiplie
- * @y: the second number to multiplie with the first number
+ * @a: the first number to multiplie
+ * @b: the second number to multiplie with the first number
  * Retran: the result of multiplication between x param and y param
  */
 
@@ -11,4 +11,5 @@ int mul(int a, int b)
 
 {
 	return (a * b);
+
 }
