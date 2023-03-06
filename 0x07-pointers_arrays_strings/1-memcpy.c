c8a0e6a35289a5d9821d78a1f2978fc7e4e284d7
Dubemxolisi <dubemxolisi04@gmail.com>
2023-03-06 03:55:39 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+                dest[i] = src[i];
+
+        return (dest);
+}
